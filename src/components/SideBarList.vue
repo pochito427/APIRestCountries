28b3864d7f24453e2431f6b2d16daaa85481f64f
Alfonso Neil Jimenez Casallas <djalfo18@gmail.com>
2023-03-14 11:12:23 -0300

    Fix URL and props from new API Fix URL and props from new API following the docs from https://restcountries.com/

--- src/components/SideBarList.vue
+++ src/components/SideBarList.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="sideBarList">
     <div class="ui visible left vertical sidebar menu">
-      <a class="item" v-for="country in countries.data" :key="country.id" @click="CountrySelected(country.name)">
-        <img class="ui avatar image" :src="country.flag" alt>
-        {{ country.name }}
+      <a
+        class="item"
+        v-for="country in countries.data"
+        :key="country.cca2"
+        @click="CountrySelected(country.name.common)"
+      >
+        <img class="ui avatar image" :src="country.flag" alt />
+        {{ country.name.common }}
       </a>
     </div>
     <div class="pusher">
@@ -11,23 +16,28 @@
         <div v-if="countrySelectedList" class="countryTable">
           <h1
             class="ui header center aligned page grid"
-            style="font-size: 40px; padding-right:20px;"
-          >{{ countryName }}</h1>
-          <table class="ui selectable inverted fixed celled table column twelve wide">
+            style="font-size: 40px; padding-right: 20px"
+          >
+            {{ countryName }}
+          </h1>
+          <table
+            class="ui selectable inverted fixed celled table column twelve wide"
+          >
             <thead>
               <tr>
                 <th class="three wide">Capital</th>
-                <th class="three wide">Región</th>
-                <th class="three wide">Gentilicio</th>
-                <th class="three wide">Nombre Nativo</th>
+                <th class="three wide">Region</th>
+                <th class="three wide">Demonym</th>
               </tr>
             </thead>
-            <tbody v-for="countrySelected in countrySelectedList.data" :key="countrySelected.id">
+            <tbody
+              v-for="countrySelected in countrySelectedList.data"
+              :key="countrySelected.cca2"
+            >
               <tr>
-                <td>{{ countrySelected.capital }}</td>
+                <td>{{ countrySelected.capital[0] }}</td>
                 <td>{{ countrySelected.region }}</td>
-                <td>{{ countrySelected.demonym }}</td>
-                <td>{{ countrySelected.nativeName }}</td>
+                <td>{{ countrySelected.demonyms.eng.f }}</td>
               </tr>
             </tbody>
           </table>
@@ -36,7 +46,7 @@
           <img
             src="https://cdn2.traveler.es/uploads/images/thumbs/es/trav/3/s/2018/28/un_fin_de_semana_para_viajar_o_para_disfrutar_de_la_ciudad_llena_de_planes_diferentes_6962_940x627.jpg"
             alt
-          >
+          />
         </div>
       </div>
     </div>
@@ -50,25 +60,25 @@ export default {
   name: "SideBarList",
   created() {
     axios
-      .get("https://restcountries.eu/rest/v2/")
-      .then(response => (this.countries = response));
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => (this.countries = response));
   },
   data() {
     return {
       countries: null,
       countrySelectedList: null,
-      countryName: null
+      countryName: null,
     };
   },
   methods: {
     CountrySelected(countryName) {
       axios
-        .get("https://restcountries.eu/rest/v2/name/" + countryName)
-        .then(response => (this.countrySelectedList = response))
+        .get("https://restcountries.com/v3.1/name/" + countryName)
+        .then((response) => (this.countrySelectedList = response))
         .then(() => {
           this.countryName = countryName;
         });
-    }   
+    },
   },
 };
 </script>
